5b42aefcdd9f75b30c77e0aed9f2339a31071a67
timwang <61922079@qq.com>
2023-07-13 20:09:24 +0800

    update output dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  buildDir: 'dist',
+  // buildDir: 'dist',
   devtools: { enabled: true },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
 })
