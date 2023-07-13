779839ce7ee7643a00dd0b987c20c843d7e0b348
timwang <61922079@qq.com>
2023-07-13 19:54:18 +0800

    remove .nuxt folder

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module "C:/Projects/smartplaybook/node_modules/.pnpm/nuxt@3.6.2_@types+node@18.0.0_sass@1.63.6/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
